3694ad866bfbb4353916ea821d92e0a161a7507e
ViRUSjE <miloszkraska12@gmail.com>
2024-02-04 18:43:47 +0100

    Data types II printing values of variables wiht names

---
+++ Task_3.py
@@ -0,0 +1,19 @@
+"Data types II"
+
+a = 134
+b = 167.67
+c = "This is string"
+d = True
+e = False
+
+"Function print value of variables"
+def print_result():
+    print(f'Variable a has value {a}')
+    print(f'Variable b has value {b}')
+    print(f'Variable c has value {c}')
+    print(f'Variable d has value {d}')
+    print(f'Variable e has value {e}')
+
+
+if __name__ == '__main__':
+    print_result()
