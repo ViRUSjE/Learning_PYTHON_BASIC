6e412a2cf64eca0f2951a44cf71bb6225c4f43df
ViRUSjE <miloszkraska12@gmail.com>
2024-02-04 17:17:31 +0100

    First task with function print

---
+++ Zad1.py
@@ -0,0 +1,14 @@
+"These are variables to use in function"
+name = "Milosz"
+surname = "Kraska"
+sentence = "I want to become a programmer and work in your company, that's why I'm creating this portfolio in PyCharm by myself."
+wishes = "Have a nice day"
+
+
+"""This function says hello, name and surname"""
+def say_hello(a, b, c, d):
+    print(f' Hi, I am {a} {b}. {c} \n {d}!')
+
+
+if __name__ == '__main__':
+    say_hello(name, surname, sentence, wishes)
