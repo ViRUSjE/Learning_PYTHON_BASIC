fc7125cefeb659a196db2c22b63c2be441a8945f
ViRUSjE <miloszkraska12@gmail.com>
2024-02-25 21:46:21 +0100

    Incrementation and decrementation

---
+++ Task_11.py
@@ -0,0 +1,24 @@
+"Incrementation and decrmentation"
+
+a = 10
+
+"""Function shows how increment works
+
+:rtype int
+"""
+def incrementation(a):
+    a +=1
+    return a
+
+
+"""Function shows how increment works
+
+:rtype int
+"""
+def decrementation(a):
+    a-= 1
+    return a
+
+
+print(decrementation(a))
+print(incrementation(a))
