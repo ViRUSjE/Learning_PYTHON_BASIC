165e64d489cea3fdeb6e7da6221373d7a49ed234
ViRUSjE <miloszkraska12@gmail.com>
2024-02-27 14:18:22 +0100

    "Compare numbers"

--- Task_19.py
+++ Task_19.py
@@ -1,7 +1,7 @@
 "Compare numbers"
 
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
 
 if a > b:
     print("a is greater")
