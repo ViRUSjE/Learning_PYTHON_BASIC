8d97179ebc01216a528bc29a1d4c7804503690c5
ViRUSjE <miloszkraska12@gmail.com>
2024-02-27 13:45:17 +0100

    "Compare names"

---
+++ Task_18.py
@@ -0,0 +1,11 @@
+"Compare names"
+
+"This program compares your names"
+
+first_name = str(input("Enter your name: ")).lower()
+second_name  = str(input("Enter your second name: ")).lower()
+
+if first_name == second_name:
+    print("Same")
+else:
+    print("Different")
